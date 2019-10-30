320ffddeb8ed72db9a67fee840c58d62caf6e83a
Abrar74774 <abrarshahriarhossain@gmail.com>
2019-10-30 10:47:50 +0300

    second commit

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ console.log("hello");
 var overlay = document.getElementById("moberlay");
 var menu = document.getElementById("mobmenu");
 var sidebar = document.getElementById("sidebar");
+var logo = document.getElementById("logo");
+
+function unfade(element) {
+  var op = 0.01;  // initial opacity
+  element.style.display = 'block';
+  var timer = setInterval(function () {
+      if (op >= 1){
+          clearInterval(timer);
+      }
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op += op * 0.1;
+  }, 25);
+}
+
+unfade(logo);
 
 menu.addEventListener("click", function(){
   overlay.style.opacity = "0.5";
