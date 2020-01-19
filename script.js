f6a72e97460c4d89a728ad27237a9ba2548cfd59
Abrar74774 <abrarshahriarhossain@gmail.com>
2020-01-19 23:02:38 +0300

    fixed responsiveness issues

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ var menu = document.getElementById("mobmenu");
 var sidebar = document.getElementById("sidebar");
 var logo = document.getElementById("logo");
 
+// function unfade(element) {
+//   var op = 0.01;  // initial opacity
+//   element.style.display = 'block';
+//   var timer = setInterval(function () {
+//       if (op >= 1){
+//           clearInterval(timer);
+//       }
+//       element.style.opacity = op;
+//       element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//       op += op * 0.1;
+//   }, 20);
+// }
+
 function unfade(element) {
-  var op = 0.01;  // initial opacity
-  element.style.display = 'block';
-  var timer = setInterval(function () {
-      if (op >= 1){
-          clearInterval(timer);
-      }
-      element.style.opacity = op;
-      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-      op += op * 0.1;
-  }, 25);
+  element.style.opacity = '1';
 }
 
 unfade(logo);
